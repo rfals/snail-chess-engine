4f67a80a7ff98499c0f20614c8819efdb427c8cf
rfals <91599078+rfals@users.noreply.github.com>
2022-09-13 22:26:19 +0300

    backend babysteps

---
+++ ChessEngine.py
@@ -0,0 +1,20 @@
+"""
+Class responsible for storing info and determining legal moves.
+"""
+
+class GameState():
+    def __init__(self) -> None:
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"], # Backrank black pieces
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"], # Black pawns
+            ["--", "--", "--", "--", "--", "--", "--", "--"], # Empty squares
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+
+        self.whiteToMove = True
+        self.moveLog = []
+        pass
